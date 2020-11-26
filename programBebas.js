cc4b6924137597193ee0aebb364d8502256b048a
Firman Azhar Riyadi <33318547+FirmanAzharR@users.noreply.github.com>
2020-11-26 13:27:02 +0700

    tugas ke 1 minggu 3

--- programBebas.js
+++ programBebas.js
@@ -1,22 +1,22 @@
 //cek jam kerja
 const cekJamKerja = (time) =>{
     return new Promise ((resolve,reject)=>{
-
-        let hoursMinutes = time.split(/[.:]/);
-        let hours = parseInt(hoursMinutes[0], 10);
-        let minutes = hoursMinutes[1] ? parseInt(hoursMinutes[1], 10) : 0;
-        let jam = hours + minutes / 60;
-
-        jam>=8 && jam<=16 ?  resolve(time) : reject(new Error(`pukul ${time} bukan jam kerja`));
+        if(typeof(time)=='string'){
+            let hoursMinutes = time.split(/[.:]/);
+            let hours = parseInt(hoursMinutes[0], 10);
+            let minutes = hoursMinutes[1] ? parseInt(hoursMinutes[1], 10) : 0;
+            let jam = hours + minutes / 60;
+            jam>=8 && jam<=16 ?  resolve(`pukul ${time} adalah jam kerja`) : reject(new Error(`pukul ${time} bukan jam kerja (contoh input 11:30)`));
+        }else{
+            reject('input harus string');    
+        }
         
     })
 }
 
-cekJamKerja('12.59')
+cekJamKerja('11:30')
 .then((result)=>{
-    console.log(
-        `pukul ${result} adalah jam kerja`
-    );
+    console.log(result);
 })
 .catch((error)=>{
     console.log(error);
@@ -25,32 +25,42 @@ cekJamKerja('12.59')
 //================================================================================================================
 
 //Cek Vitamin dalam buah
-const cekVitamin = (buah) =>{
+const cekBuah = (buah) =>{
     return new Promise((resolve, reject)=>{
         const dataBuah =[
-            { nama: 'Apel', vitamin: 'A,C'},
-            { nama: 'Jeruk', vitamin: 'C'}
+            { nama: 'Apel', vitamin: 'A,B1,B2,C'},
+            { nama: 'Jeruk', vitamin: 'A,B1,B2,C'},
+            { nama: 'Pisang', vitamin: 'A,B1,B2'},
+            { nama: 'Mangga', vitamin: 'A,E,C'},
+            { nama: 'Tomat', vitamin: 'A,B1,C'},
         ];
-        let cek = dataBuah.filter(
-            x => x.nama.toLowerCase() === buah.toLowerCase()
-        );
-        if(cek.length>0){
-            resolve(cek);
+        if(typeof(buah)=='string'){
+            let cek = dataBuah.filter(
+                x => x.nama.toLowerCase() === buah.toLowerCase()
+                );
+            if(cek.length>0){
+                resolve(
+                    cek//.map(x=>console.log(`Buah ${x.nama} mengandung vitamin ${x.vitamin}`))
+                );
+            }else{
+                reject(new Error('Buah tidak terdaftar dalam list'));
+            }
         }else{
-            reject(new Error('Buah tidak terdaftar dalam list'));
+            reject('Inputan harus string');
         }
     })
 }
-
-cekVitamin('apel')
+        
+cekBuah('mangga')
 .then((result)=>{
-    //console.log(result);
+    //result;
     result.map(x=>console.log(`Buah ${x.nama} mengandung vitamin ${x.vitamin}`))
 })
 .catch((error)=>{
     console.log(error);
 })
-
+        
 //===============================================================================================================
-
-
+        
+        
+        
